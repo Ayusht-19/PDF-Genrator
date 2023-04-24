c575dedb34905ec2b44b5c69776867e153ae5a70
Ayush Trivedi <ayushtrivedi92@gmail.com>
2023-04-24 23:50:31 +0530

    Added Multiple pages in pdf

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ for index,row in df.iterrows():
 
     pdf.set_font(family="Times",style="B", size=20)
     pdf.cell(w=0,h=12,txt=row["Topic"],align="l")
-    pdf.line(x1=10,y1=20,x2=200,y2=20)
+    pdf.line(10, 20, 200, 20)
+
+    for i in range(row["Pages"] -1):
+        pdf.add_page()
 
 pdf.output("output.pdf")
